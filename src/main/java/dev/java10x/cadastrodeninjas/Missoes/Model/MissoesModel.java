e6eb0f28f904cf72314c72a80f12540510f2aee9
Adeilton da Silva Polovodoff <paraminhaalegria000@gmail.com>
2024-10-28 18:03:47 -0300

    fix: esse commit adiciona a coluna de email como unique

--- src/main/java/dev/java10x/cadastrodeninjas/Missoes/Model/MissoesModel.java
+++ src/main/java/dev/java10x/cadastrodeninjas/Missoes/Model/MissoesModel.java
@@ -2,15 +2,16 @@ package dev.java10x.cadastrodeninjas.Missoes.Model;
 
 import dev.java10x.cadastrodeninjas.Ninjas.controller.model.NinjaModel;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 
-@Getter
-@Setter
+
 @Entity
 @Table(name="tb_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MissoesModel {
 
     @Id
@@ -23,15 +24,7 @@ public class MissoesModel {
     @OneToMany(mappedBy = "missoes") // @OneToMany  uma missao tem varios ninjas
     private List<NinjaModel> ninja;
 
-
-
-
-    public MissoesModel() {
-    }
-
-    public MissoesModel(Long id, String nome, String dificuldade) {
-        this.id = id;
-        this.nome = nome;
-        this.dificuldade = dificuldade;
-    }
 }
+
+
+
--- src/main/java/dev/java10x/cadastrodeninjas/Ninjas/controller/model/NinjaModel.java
+++ src/main/java/dev/java10x/cadastrodeninjas/Ninjas/controller/model/NinjaModel.java
@@ -3,35 +3,29 @@ package dev.java10x.cadastrodeninjas.Ninjas.controller.model;
 
 import dev.java10x.cadastrodeninjas.Missoes.Model.MissoesModel;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 
-@Getter
-@Setter
 @Entity
 @Table(name="tb_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
+
+    @Column(unique = true)
     private String email;
+
     private int idade;
 
     @ManyToOne // @ManyToOne  um ninja tem uma unica missao
     @JoinColumn(name = "missoes_id") //ForeignKey que faz a ligação entre as tabelas Ninja e Missoes pelo id da missao
     private MissoesModel missoes;
-
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
 
 }
